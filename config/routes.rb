adaf86ee97822b2d9aad791fa2d31b086b1fd85f
Joe Guerra <merlin2049er@gmail.com>
2022-07-16 13:50:50 -0400

    devise and magiclink

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
+
+  devise_for :users,
+     controllers: { sessions: "devise/passwordless/sessions" }
+   devise_scope :user do
+     get "/users/magic_link",
+       to: "devise/passwordless/magic_links#show",
+       as: "users_magic_link"
+   end
+
 end
